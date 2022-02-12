0cd3ec3a917107e6f444ac5292f11b6bfb0a7554
naydin2 <36387111+naydin2@users.noreply.github.com>
2022-02-11 23:07:35 -0500

    encryption bot commit
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- public_s3_copy.tf
+++ public_s3_copy.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "b" {
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
   bucket = "my-tf-test-bucket"
   acl    = "public-read"
 
